fbca1e95b9f73f9bd1d31b9d0907e213293e1ea0
Manikanta-12345 <suryamutyala757@gmail.com>
2021-08-17 21:59:16 +0530

    first commit

---
+++ src/main/java/com/tech/apps/BootJenkinsPipeLineApplication.java
@@ -0,0 +1,13 @@
+package com.tech.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJenkinsPipeLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJenkinsPipeLineApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tech/apps/JenkinsController.java
@@ -0,0 +1,15 @@
+package com.tech.apps;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController extends SpringBootServletInitializer {
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String welcomeJenkins() {
+		return "Welcome To SpringBoot Jenkins PipeLine";
+	}
+}
